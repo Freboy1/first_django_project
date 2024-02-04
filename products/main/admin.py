29e8e7ae61c049deee79950d59d38cc21896be4d
freboy1 <dautovalisher33@gmail.com>
2024-02-04 14:16:39 +0600

    v2

--- products/main/admin.py
+++ products/main/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 # Register your models here.
 
-admin.site.register(Product)
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+
+admin.site.register(Product, ProductAdmin)
--- products/main/models.py
+++ products/main/models.py
@@ -4,4 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='media/')
